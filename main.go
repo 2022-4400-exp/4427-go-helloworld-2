8577096fd4e794fdd5bf2cc4543cb3f27c9867b6
rarun0705 <m19082@g.metro-cit.ac.jp>
2022-12-20 14:53:23 +0900

    main.go

--- main.go
+++ main.go
@@ -16,13 +16,14 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.String(http.StatusOK, "4427 : busy")
+        //return c.String(http.StatusOK, "4427 : busy")
+        return c.String(http.StatusOK, "4427: I don't want to do anything ver.2")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        //return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4427", "message": "busy"})
-        return c.String(http.StatusOK, "4427: I don't want to do anything ver.2")
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4427", "message": "busy"})
+        
     }
 }
