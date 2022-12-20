6c369e1a7f039baad29edfbbf9301016ef32ca83
rarun0705 <m19082@g.metro-cit.ac.jp>
2022-12-20 14:14:41 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "4427 : busy")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        //return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4427", "message": "busy"})
+        return c.String(http.StatusOK, "4427: I don't want to do anything ver.2")
+    }
+}
